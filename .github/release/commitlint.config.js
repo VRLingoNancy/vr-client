96d9501c13e162d25c000e588d20f144b3e26121
Jean-Baptiste REVEL <jean-baptiste.revel@epitech.eu>
2025-09-19 13:33:59 +0200

    build: add basic CI

---
+++ .github/release/commitlint.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    // Project specific rules
+    'scope-enum': [2, 'always', ['ci']],
+
+    // Compliant with Angular conventions (get rid of "style" type but tolerate "chore" one)
+    'type-enum': [2, 'always', ['feat', 'fix', 'perf', 'revert', 'refactor', 'build', 'test', 'ci', 'docs', 'chore']],
+
+    // Adjust some rules predefined by @commitlint/config-conventional
+    'body-max-line-length': [0, 'always', 'Infinity'],
+    'footer-max-line-length': [0, 'always', 'Infinity']
+  }
+}
