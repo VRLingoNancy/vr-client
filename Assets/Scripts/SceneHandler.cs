b3d184c9766c8910ebd1cf192a54ed770b840310
Yoni Rigaut <yoni.rigaut@epitech.eu>
2025-11-13 15:15:35 +0100

    changes: added new main scene and deleted basic sample scenes in unity

---
+++ Assets/Scripts/SceneHandler.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class SceneHandler : MonoBehaviour
+{
+    public CanvasGroup fadeCanvas;
+    public float fadeDuration = 1f;
+
+    public void LoadSceneWithFade(string sceneName)
+    {
+        StartCoroutine(FadeAndLoad(sceneName));
+    }
+
+    private IEnumerator FadeAndLoad(string sceneName)
+    {
+        float t = 0;
+        while (t < fadeDuration)
+        {
+            t += Time.deltaTime;
+            fadeCanvas.alpha = t / fadeDuration;
+            yield return null;
+        }
+        SceneManager.LoadScene(sceneName);
+    }
+}
